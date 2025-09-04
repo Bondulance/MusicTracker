bb91a3097f1217feeb3130f77afe975d290589e9
Bodhi Woll <144378333+Bondulance@users.noreply.github.com>
2025-09-04 10:05:45 -0400

    styles / scripts

--- components/Header.php
+++ components/Header.php
@@ -6,4 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Music Tracker | <?php echo $pageTitle ?></title>
     <link rel="stylesheet" href="./lib/styles/styles.css">
+    <script src="./lib/scripts/utils.js" async></script>
 </head>
+
+<body>
+    <header>
+        <nav>
+            <h1>Music Tracker</h1>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -3,6 +3,5 @@ $pageTitle = 'Homepage';
 include('./components/Header.php');
 ?>
 
-<body>
-    <h1>PHP</h1>
+
 </body>
